482a0be6ea9ed7934e9db6bc6e2565b15c23369e
Matthew Reidy <matthewreidy5@gmail.com>
2024-03-02 17:05:35 -0800

    reformat and progress made

--- main.go
+++ main.go
@@ -1,134 +1,67 @@
 package main
 
 import (
-	"errors"
+	"fmt"
 	"log"
 	"net"
 	"os"
-	"regexp"
+	"time"
 )
 
 const (
-	httpProtVer  = "HTTP/1.1"
-	srvr         = "matts server"
-	rootFilePath = "/www/http/src"
+	httpVer = "HTTP/1.1"
+	rateLim = 10
+	port    = 8080
 )
 
-type HTTPResponse struct {
-	respCode    string
-	contentType string
-	contentLen  string
-	body        string
-}
+func openDoc(document string) string {
+	file, err := os.Open("www/" + document)
 
-type request struct {
-	identity net.Conn
-	method   string
-	resource string
-	httpVer  string
-}
-
-func fileOrPathExists(resource string) bool {
-	fullPath := rootFilePath + resource
-	_, err := os.Open(fullPath)
+	fileContents := make([]byte, 150)
 
 	if err != nil {
-		log.Panicln("error!", err)
-		return false
-	}
-
-	return true
-
-}
-
-func buildResponse(responseObj *HTTPResponse) []byte {
-	respString := ""
-
-	return []byte(respString)
-}
-
-func handleRequest(reqChan chan *request) {
-	req := <-reqChan
-	conn := req.identity
-	response := &HTTPResponse{}
-
-	if !fileOrPathExists(req.resource) {
-		response.respCode = "404 Not Found"
-		conn.Write(buildResponse(response))
-		conn.Close()
-	} else {
-		response.respCode = "200 OK"
-	}
-}
-
-func parseRequest(conn net.Conn, inputStream *[]byte) (*request, error) {
-
-	req := &request{identity: conn, method: "", resource: "", httpVer: ""}
-	expressions := []string{`^(GET|POST|PUT|DELETE|OPTIONS)`, `\s\/[^\s]+`, `HTTP\/[^\s]+`}
-
-	for i := range expressions {
-		re := regexp.MustCompile(expressions[i])
-		switch i {
-		case 0:
-
-			method := string(re.Find(*inputStream))
-			if method != "GET" {
-				return nil, errors.New("server does not support this request type")
-			}
-			req.method = string(re.Find(*inputStream))
-
-		case 1:
-
-			resource := string(re.Find(*inputStream))
-			req.resource = resource
-
-		case 2:
-
-			version := string(re.Find(*inputStream))
-
-			if version != httpProtVer {
-				return nil, errors.New("can not accept requests from this version of http")
-			}
-			req.httpVer = version
-
-		}
+		log.Panicln("Can not opne file!", err)
 
 	}
+	n, _ := file.Read(fileContents)
 
-	return req, nil
+	file.Close()
+
+	return string(fileContents[0:n])
+
 }
 
-func handleConnection(conn net.Conn, reqChan chan *request) {
-	inputStream := make([]byte, 1042)
+func handleResponse(connCh chan net.Conn) {
+	conn := <-connCh
+	currTime := time.Now()
+	file := openDoc("index.html")
+	conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nDate:%+v\r\nServer: Matts Srvr\r\nContent-Type:text/html\r\nContent-Length:%d\r\n\r\n%s", currTime.Format("Sat, 06 Feb 2024 12:00:00"), len(file), file)))
+	conn.Close()
 
-	_, err := conn.Read(inputStream)
+}
+
+func handleRequest(conn net.Conn, connCh chan net.Conn) {
+
+	requestStream := make([]byte, 1042)
+
+	_, err := conn.Read(requestStream)
 
 	if err != nil {
 		log.Panicln(err, "error in reading request")
 	}
-
-	parsed, err := parseRequest(conn, &inputStream)
-
-	if err != nil {
-		log.Fatalln("FATAL", err)
-	}
-
-	reqChan <- parsed
-
+	connCh <- conn
 }
 
 func main() {
 
-	ln, err := net.Listen("tcp", "localhost:8080")
+	ln, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
 
 	log.Println("lisenting on", ln.Addr().String())
 
 	if err != nil {
 		log.Fatal("can not listen...closing connection", err)
 	}
-
-	reqChan := make(chan *request) //request channel
-
+	connCh := make(chan net.Conn)
 	for {
 
 		conn, err := ln.Accept()
@@ -138,8 +71,8 @@ func main() {
 			return
 		}
 
-		go handleConnection(conn, reqChan)
-		go handleRequest(reqChan)
+		go handleRequest(conn, connCh)
+		go handleResponse(connCh)
 
 	}
 }
